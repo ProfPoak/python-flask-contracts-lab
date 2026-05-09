18cc5935981ad7f36b6d0dc502655e0d9e0fb19d
Porter <pdhakes25@gmail.com>
2026-05-09 10:48:40 -0600

    Implemented contract and customer responses

--- server/app.py
+++ server/app.py
@@ -6,5 +6,32 @@ contracts = [{"id": 1, "contract_information": "This contract is for John and bu
 customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
+@app.route('/contract/<int:id>')
+def contract(id):
+    match = next((contract for contract in contracts if contract["id"] == id), None)
+    if match:
+        status_code = 200
+        headers = {}
+        return make_response(match["contract_information"], status_code, headers)
+    
+    response_body = f"Contract with ID:{id} not found"
+    status_code = 404
+    headers = {}
+    return make_response(response_body, status_code, headers)
+
+@app.route('/customer/<string:customer_name>')
+def customer(customer_name):
+    if customer_name in customers:
+        response_body = ""
+        status_code = 204
+        headers = {}
+        return make_response(response_body, status_code, headers)
+    
+    response_body = f"Customer {customer_name} not found"
+    status_code = 404
+    headers = {}
+    return make_response(response_body, status_code, headers)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
